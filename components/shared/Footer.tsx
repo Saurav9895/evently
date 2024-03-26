42eb658bbc3c8eecfe31c4d5c48a978135504374
Saurav Yadav <93671987+Saurav9895@users.noreply.github.com>
2024-03-26 15:12:34 +0530

    first commit

---
+++ components/shared/Footer.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const Footer = () => {
+  return (
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center justify-center">
+      
+
+        <p>2024 Evently. All Rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
